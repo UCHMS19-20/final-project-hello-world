28f80563983ddabce242d2331f5b12b6f068104d
Jcondez <jcondez@ucvts.org>
2020-01-03 10:05:23 -0500

    Added beginning story

--- game.py
+++ game.py
@@ -1,2 +1,10 @@
 import sys
 import pygame
+
+#start screen with input name 
+#intro screen with introduction to commands
+    #commands such as look, talk, touch, move to
+#introduce lil character and main problem
+#setting unknown to player, everything is new
+#Task: You are a robot who just woke up, but can't find who created them
+    #Lead them to their owner
